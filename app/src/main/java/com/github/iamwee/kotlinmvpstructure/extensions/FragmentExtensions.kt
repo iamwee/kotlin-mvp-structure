23ed81133cbb53c0050b8f1de5393ade810c63d2
iamwee <wathin.v@gmail.com>
2017-10-10 20:31:00 +0700

    remove overload function just use a default value of argument function

--- app/src/main/java/com/github/iamwee/kotlinmvpstructure/extensions/FragmentExtensions.kt
+++ app/src/main/java/com/github/iamwee/kotlinmvpstructure/extensions/FragmentExtensions.kt
@@ -9,19 +9,7 @@ import com.github.iamwee.kotlinmvpstructure.R
  * Created by zeon on 10/9/2017 AD.
  */
 
-fun Fragment.add(fragmentManager: FragmentManager) {
-    try {
-        fragmentManager.beginTransaction()
-                .add(R.id.container, this)
-                .commit()
-    } catch (exception: IllegalStateException) {
-        fragmentManager.beginTransaction()
-                .add(R.id.container, this)
-                .commitAllowingStateLoss()
-    }
-}
-
-fun Fragment.add(fragmentManager: FragmentManager, savedInstanceState: Bundle?) {
+fun Fragment.add(fragmentManager: FragmentManager, savedInstanceState: Bundle? = null) {
     if (savedInstanceState == null) {
         try {
             fragmentManager.beginTransaction()
@@ -35,19 +23,7 @@ fun Fragment.add(fragmentManager: FragmentManager, savedInstanceState: Bundle?) 
     }
 }
 
-fun Fragment.replace(fragmentManager: FragmentManager) {
-    try {
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, this)
-                .commit()
-    } catch (exception: IllegalStateException) {
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, this)
-                .commitAllowingStateLoss()
-    }
-}
-
-fun Fragment.replace(fragmentManager: FragmentManager, savedInstanceState: Bundle?) {
+fun Fragment.replace(fragmentManager: FragmentManager, savedInstanceState: Bundle? = null) {
     if (savedInstanceState == null) {
         try {
             fragmentManager.beginTransaction()
